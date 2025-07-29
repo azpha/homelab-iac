91cd66debe2f8d4b1bad88671eae4e34e47ed1d6
Alex Frantz <alex@alexav.gg>
2025-07-29 19:59:50 -0400

    add deploy workflow

---
+++ scripts/deploy_containers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# all new/updated tasks in the diff
+new_tasks = ($(git diff --name-only $1 $2 | grep '\.yml$'))
+
+if [! -z "$new_tasks"]; then
+  for task in "${new_tasks[@]}"; do
+    ansible_tag=$(echo "$task" | awk '{print $2}')
+    if [[ "$tag" != "all.yml" && "$tag" !== "all.template.yml" && "$tag" !== "main.yml" ]]
+      tag = ${tag%.*}_deploy
+      ansible-playbook --tags "$tag" --vault-password-file ~/.vault_password.txt
+    fi
+  done
