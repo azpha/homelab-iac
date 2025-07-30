112c4c34bfda4175a7b48305f869a5c2a17e9f10
Alex Frantz <alex@alexav.gg>
2025-07-29 21:33:52 -0400

    wasn't catching the container tag

--- scripts/deploy_containers.sh
+++ scripts/deploy_containers.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # all new/updated tasks in the diff
-echo $1 $2
-
 new_tasks=($(git diff --name-only $1 $2 | grep '\.yml$'))
+echo $new_tasks
+echo $1 $2
 
 if [ ! -z "$new_tasks" ]; then
   for task in "${new_tasks[@]}"; do
-    ansible_tag=$(echo "$task" | awk '{print $2}')
+    ansible_tag=$(echo "$task" | awk -F/ '{print $2}')
     if [[ "$tag" != "all.yml" && "$tag" != "all.template.yml" && "$tag" != "main.yml" ]] ; then
-      tag=${tag%.*}_deploy
+      tag=${ansible_tag%.*}_deploy
+      echo $tag
       ansible-playbook main.yml --tags "$tag" --vault-password-file ~/.vault_pass.txt
     fi
   done
