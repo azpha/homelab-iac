c8cceee3a95a232ae78eb3d94e819b0b85292376
Alex Frantz <alex@alexav.gg>
2025-07-29 21:16:54 -0400

    move gitea to role, fix deploy script

--- scripts/deploy_containers.sh
+++ scripts/deploy_containers.sh
@@ -3,13 +3,14 @@
 # all new/updated tasks in the diff
 echo $1 $2
 
-new_tasks = ($(git diff --name-only $1 $2 | grep '\.yml$'))
+new_tasks=($(git diff --name-only $1 $2 | grep '\.yml$'))
 
-if [! -z "$new_tasks"]; then
+if [ ! -z "$new_tasks" ]; then
   for task in "${new_tasks[@]}"; do
     ansible_tag=$(echo "$task" | awk '{print $2}')
-    if [[ "$tag" != "all.yml" && "$tag" !== "all.template.yml" && "$tag" !== "main.yml" ]]
-      tag = ${tag%.*}_deploy
-      ansible-playbook --tags "$tag" --vault-password-file ~/.vault_password.txt
+    if [[ "$tag" != "all.yml" && "$tag" != "all.template.yml" && "$tag" != "main.yml" ]] ; then
+      tag=${tag%.*}_deploy
+      ansible-playbook ../main.yml --tags "$tag" --vault-password-file ~/.vault_password.txt
     fi
   done
+fi
