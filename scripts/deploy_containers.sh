9a77e7bed646d7343916b5e49dff2bbffe839feb
Alex Frantz <alex@alexav.gg>
2025-07-31 01:30:44 -0400

    redeploy caddy when vars change

--- scripts/deploy_containers.sh
+++ scripts/deploy_containers.sh
@@ -12,6 +12,8 @@ if [ ! -z "$new_tasks" ]; then
       if [[ "$tag" != "_deploy" ]] ; then 
         ansible-playbook main.yml --tags "$tag" --vault-password-file ~/.vault_pass.txt
       fi
+    elif [[ "$ansible_tag" == "jade.yml" || "$ansible_tag" == "jackson.yml" ]] ; then
+      ansible-playbook main.yml --tags "caddy_deploy" --vault-password-file ~/.vault_pass.txt
     fi
   done
 fi
