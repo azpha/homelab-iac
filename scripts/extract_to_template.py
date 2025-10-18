a990c7bc190db99b0c7191da0e44fc3368da2b12
Alex Frantz <alex@alexav.gg>
2025-10-18 18:13:41 -0400

    add template extraction

---
+++ scripts/extract_to_template.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+
+host_vars_path = os.path.abspath('host_vars')
+file_contents = ""
+
+if os.path.exists(host_vars_path):
+  vaults = os.listdir(host_vars_path)
+
+  print(vaults)
+  for vault in vaults:
+    vault_path = os.path.join(host_vars_path, vault)
+    print(f'ansible-vault decrypt "{vault_path}" --vault-password-file ~/.vault_pass.txt')
+    vault_contents = subprocess.run(f'ansible-vault decrypt "{vault_path}" --vault-password-file ~/.vault_pass.txt --output -', shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    stdout = vault_contents.stdout.strip().splitlines()
+
+    for line in stdout:
+      if line.startswith("#") and line not in file_contents:
+        file_contents += f"\n{line}\n"
+
+      if ":" in line:
+        if line.split(":")[0] not in file_contents:
+          file_contents += f'{line.split(":")[0]}:\n'
+
+    with open(os.path.join(host_vars_path, 'all.template.yml'), 'w', encoding="utf8") as template_file:
+      template_file.write(file_contents)
+      template_file.close()
+
+    print("Written to disk!")
